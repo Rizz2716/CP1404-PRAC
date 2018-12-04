c70064654f5b87d847473a996571545a7e1581e8
Rizz2716 <41773993+Rizz2716@users.noreply.github.com>
2018-12-04 13:11:25 +0800

    PRACTICALS UPTO 20 NOV

---
+++ practical 5/inclas.py
@@ -0,0 +1,9 @@
+# def main():
+#     list_names = ['Alex', 'Burnie', 'Chad']
+#     list_ages = ['23', '32', '16']
+#     print(list_names[list_ages.index(max(list_ages))])
+# main()
+
+words = ["aye", "bee", "sea", "bee"]
+words.remove("bee")
+print(words.pop())
